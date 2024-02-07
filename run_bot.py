399ca3edfa10499d54af712484aeb96ac071267f
piotrb <piotrbanko@me.com>
2024-02-08 00:13:59 +0100

    Load the SERVICE_GROUP for the config file instead of env variable

--- run_bot.py
+++ run_bot.py
@@ -8,10 +8,9 @@ from telegram_assistant import TelegramAssistant
 from data import *
 
 if __name__ == '__main__':
-    group = os.getenv('GROUP')
-    bot = TelegramAssistant(SESSION_FILE, SESSIONS_FOLDER, API_ID, API_HASH, [group], group, ASSISTANT_ID,
-                            THREAD_ID)
+    bot = TelegramAssistant(SESSION_FILE, SESSIONS_FOLDER, API_ID, API_HASH, [SERVICE_GROUP], SERVICE_GROUP,
+                            ASSISTANT_ID, THREAD_ID)
 
-    asyncio.get_event_loop().run_until_complete(bot.start(IP, PORT, USERNAME, PASSWORD))
+    asyncio.get_event_loop().run_until_complete(bot.start(IP, int(PORT), USERNAME, PASSWORD))
 
     asyncio.get_event_loop().run_until_complete(bot.run())
